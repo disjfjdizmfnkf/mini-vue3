a25a158681148b73130cb14370b6039a6cf588e5
disjfjdizmfnkf <3037650079@qq.com>
2024-07-26 16:15:35 +0800

    feat: reactive func

---
+++ packages/reactivity/src/baseHandlers.ts
@@ -0,0 +1,2 @@
+
+export const mutableHandlers: ProxyHandler<object> = {}
---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,32 @@
+import { mutableHandlers } from './baseHandlers'
+
+const reactiveMap = new WeakMap<object, any>
+
+
+/**
+ * 返回一个代理对象
+ * @param target 被代理对象
+ * @returns 代理对象
+ */
+export function reactive(target: object) {
+  return createReactiveObject(target, mutableHandlers, reactiveMap);
+}
+
+function createReactiveObject(
+  target: object,
+  baseHandlers: ProxyHandler<any>,
+  proxyMap: WeakMap<object, any>
+) {
+  const existingProxy = proxyMap.get(target)
+
+  if (existingProxy) {
+    return existingProxy
+  }
+
+  // 如果该对象不存在proxy对象
+  const proxy = new Proxy(target, baseHandlers)
+  // 利用哈希表缓存对象
+  proxyMap.set(target, proxy)
+
+  return proxy
+}
